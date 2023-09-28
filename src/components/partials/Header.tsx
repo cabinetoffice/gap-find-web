b713e37d12bf2cfdd2d02f8b667eb2eeffcbbceb
john-tco <john.morley@cabinetoffice.gov.uk>
2023-09-28 12:04:39 +0100

    add defaults to header props

--- src/components/partials/Header.tsx
+++ src/components/partials/Header.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import { isIE } from 'react-device-detect';
 import { skipToMainContent } from '../../utils/skipToMainContent';
 
-const Header = ({ showNavigation, showBetaBlock }) => {
+const Header = ({ showNavigation = true , showBetaBlock = true }) => {
   const router = useRouter();
   const navItems = [
     {
