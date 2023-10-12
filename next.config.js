5ff42a9823936927d91d9f91d4cf3d3238330ffe
dominicwest <dominic.west@cabinetoffice.gov.uk>
2023-10-12 11:14:54 +0100

    Fixing snyk failures

--- next.config.js
+++ next.config.js
@@ -1,14 +1,7 @@
 /** @type {import('next').NextConfig} */
 
 const path = require('path');
-const withSass = require('@zeit/next-sass');
 
-module.exports = withSass({
-  /* bydefault config  option Read For More Optios
-  here https://github.com/vercel/next-plugins/tree/master/packages/next-sass
-  */
-  cssModules: true,
-});
 module.exports = {
   serverRuntimeConfig: {
     databaseUrl: process.env.DATABASE_URL,
