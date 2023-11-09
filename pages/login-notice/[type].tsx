097951d579998013806f99cfb88e004328dab3db
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2023-11-09 12:12:17 +0000

    fix migration banner content

--- pages/login-notice/[type].tsx
+++ pages/login-notice/[type].tsx
@@ -19,17 +19,6 @@ const {
   NEWSLETTER,
 } = LOGIN_NOTICE_TYPES;
 
-// can be replaced with tickets 1976 / 1975
-const DEFAULT_CONTENT = {
-  title: 'Manage your notifications',
-  content: [
-    'To manage your notifications, you need to sign in with GOV.UK One Login.',
-    'If you do not have a GOV.UK One Login, you can create one.',
-    'If you want to unsubscribe from notifications without creating a GOV.UK One Login, you can use the unsubscribe link in the emails we send to you.',
-  ],
-  redirectUrl: notificationRoutes.manageNotifications,
-};
-
 const getNotificationContent = (action: string) => [
   `To ${action}, you need to sign in with GOV.UK One Login.`,
   'If you do not have a GOV.UK One Login, you can create one.',
@@ -42,21 +31,28 @@ export const NOTICE_CONTENT = {
       ...getNotificationContent('manage your notifications'),
       'If you want to unsubscribe from notifications without creating a GOV.UK One Login, you can use the unsubscribe link in the emails we send to you.',
     ],
-    redirectUrl: notificationRoutes.manageNotifications,
+    redirectUrl: `${notificationRoutes.manageNotifications}?migrationType=${MANAGE_NOTIFICATIONS}`,
   }),
   [SUBSCRIPTION_NOTIFICATIONS]: (ctx) => ({
     title: 'Sign up for updates',
     content: getNotificationContent('sign up for updates'),
-    redirectUrl: `${notificationRoutes.manageNotifications}?action=${URL_ACTIONS.SUBSCRIBE}&grantId=${ctx.query.grantId}`,
+    redirectUrl: `${notificationRoutes.manageNotifications}?action=${URL_ACTIONS.SUBSCRIBE}&grantId=${ctx.query.grantId}&migrationType=${SUBSCRIPTION_NOTIFICATIONS}`,
   }),
   [SAVED_SEARCH]: (ctx) => ({
     title: 'Save your search',
     content: getNotificationContent('save a search'),
     redirectUrl: `${notificationRoutes.saveSearch}?${buildQueryString(
       ctx.query,
-    )}`,
+    )}&migrationType=${SAVED_SEARCH}`,
   }),
-  [NEWSLETTER]: () => DEFAULT_CONTENT,
+  [NEWSLETTER]: () => ({
+    title: 'Get updates about new grants',
+    content: [
+      'We will only ever email you once a week. We will not email you if no new grants have been added.',
+      'If you do not have a GOV.UK One Login, you can create one.',
+    ],
+    redirectUrl: `${notificationRoutes.manageNotifications}?action=${URL_ACTIONS.NEWSLETTER_SUBSCRIBE}&migrationType=${NEWSLETTER}`,
+  }),
 } as const;
 
 export const getServerSideProps = (ctx: GetServerSidePropsContext) => {
