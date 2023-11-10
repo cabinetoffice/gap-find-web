6d12dfd0373154ab4c3cded314b6da8bddf5df35
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2023-11-10 13:52:04 +0000

    pre-render correct newsletter link when one login enabled

--- src/components/newsletter/NewsletterCallToAction.tsx
+++ src/components/newsletter/NewsletterCallToAction.tsx
@@ -1,7 +1,7 @@
 import Link from 'next/link';
 import { BellIcon } from '../icons/BellIcon/BellIcon';
 import styles from './NewsletterCallToAction.module.css';
-import { useAppContext } from '../../../pages/_app';
+import { useAppContext, useAuth } from '../../../pages/_app';
 import {
   LOGIN_NOTICE_TYPES,
   URL_ACTIONS,
@@ -12,20 +12,34 @@ type NewsletterCTAProps = {
   returnParams: URLSearchParams;
 };
 
-const getNewsletterPath = (oneLoginEnabled, returnParams) => {
-  if (oneLoginEnabled)
-    return {
-      pathname: notificationRoutes.manageNotifications,
-      query: {
-        action: URL_ACTIONS.NEWSLETTER_SUBSCRIBE,
-        migrationType: LOGIN_NOTICE_TYPES.NEWSLETTER,
-      },
-    };
+const getNewsletterPath = ({
+  oneLoginEnabled,
+  isUserLoggedIn,
+  returnParams,
+}) => {
+  if (oneLoginEnabled) {
+    if (!isUserLoggedIn)
+      return {
+        pathname: notificationRoutes.loginNotice,
+        query: {
+          action: URL_ACTIONS.SUBSCRIBE,
+          migrationType: LOGIN_NOTICE_TYPES.NEWSLETTER,
+        },
+      };
+    else
+      return {
+        pathname: notificationRoutes.manageNotifications,
+        query: {
+          action: URL_ACTIONS.NEWSLETTER_SUBSCRIBE,
+        },
+      };
+  }
   return { pathname: '/newsletter', query: returnParams };
 };
 
 const NewsletterCallToAction = ({ returnParams }: NewsletterCTAProps) => {
   const { oneLoginEnabled } = useAppContext();
+  const { isUserLoggedIn } = useAuth();
   return (
     <div className={`govuk-grid-row ${styles.withBorder}`}>
       <div className="govuk-grid-column-full gap-search-area govuk-!-padding-4">
@@ -34,7 +48,13 @@ const NewsletterCallToAction = ({ returnParams }: NewsletterCTAProps) => {
           <h3 className={`govuk-heading-s ${styles.newsletterCtaTitle}`}>
             Get updates about new grants
           </h3>
-          <Link href={getNewsletterPath(oneLoginEnabled, returnParams)}>
+          <Link
+            href={getNewsletterPath({
+              oneLoginEnabled,
+              isUserLoggedIn,
+              returnParams,
+            })}
+          >
             <a
               className={`govuk-link govuk-link--no-visited-state govuk-!-font-size-19`}
               data-cy="cySignUpNewsletter"
