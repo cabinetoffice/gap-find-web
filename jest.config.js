47b68e57c8e355079f9f7f1e70138bb1ce9eed6d
dominicwest <dominic.west@cabinetoffice.gov.uk>
2024-01-17 18:28:22 +0000

    Running jest with SWC

--- jest.config.js
+++ jest.config.js
@@ -14,6 +14,9 @@ const customJestConfig = {
   setupFilesAfterEnv: ['<rootDir>/setupTestEnv.js'],
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['<rootDir>/cypress/', '.data.js'],
+  transform: {
+    '^.+\\.(t|j)sx?$': '@swc/jest',
+  },
 };
 
 // createJestConfig is exported this way to ensure that next/jest can load the Next.js config which is async
