10a076919dcb8c5db3f05bfef08651bec52c3f0e
Jim Purvis <james.purvis@and.digital>
2024-01-18 16:44:32 +0000

    TMI2-550- Wrapping filter options in an accordion on mobile

--- src/components/search-page/search-filter-container/SearchFilterContainer.js
+++ src/components/search-page/search-filter-container/SearchFilterContainer.js
@@ -13,6 +13,7 @@ export function SearchFilterContainer({ filters, filterObj, query }) {
     );
     if ($filterAccordion && window.GOVUKFrontend !== undefined) {
       new window.GOVUKFrontend.Accordion($filterAccordion).init();
+
       // TODO: Backup option
       // if (isMobile) {
       //   document.querySelector('.govuk-accordion__controls').click();
@@ -20,14 +21,6 @@ export function SearchFilterContainer({ filters, filterObj, query }) {
     }
   }, []);
 
-  return isMobile ? (
-    <MobileContainer filters={filters} filterObj={filterObj} query={query} />
-  ) : (
-    <BaseContainer filters={filters} filterObj={filterObj} query={query} />
-  );
-}
-
-function BaseContainer({ filters, filterObj, query }) {
   return (
     <div className="govuk-grid-column-one-third">
       <div className="gap_filters govuk-!-margin-bottom-2">
@@ -44,16 +37,11 @@ function BaseContainer({ filters, filterObj, query }) {
           id="accordion-default"
           aria-label="filter options"
         >
-          {filters?.map((filter, index) => (
-            <SearchFilterSelector
-              key={index}
-              index={index}
-              filter={filter}
-              filterObj={filterObj}
-            />
-          ))}
-
-          <SearchFilterDate index={filters.length} filterObj={filterObj} />
+          {isMobile ? (
+            <SearchOptionsContainer filters={filters} filterObj={filterObj} />
+          ) : (
+            <Filters filters={filters} filterObj={filterObj} />
+          )}
         </div>
       </div>
       <div className="govuk-button-group govuk-!-margin-bottom-0 gap_filters-actions">
@@ -93,11 +81,57 @@ function BaseContainer({ filters, filterObj, query }) {
   );
 }
 
-function MobileContainer({ filters, filterObj, query }) {
+function SearchOptionsContainer({ filters, filterObj }) {
+  const index = -1;
   return (
-    // TODO: Add the collapsable container here
-    <p>
-      <BaseContainer filters={filters} filterObj={filterObj} query={query} />
-    </p>
+    <div key={index} className="govuk-accordion__section">
+      <div
+        key={index}
+        className="govuk-accordion__section govuk-accordion__section--expanded"
+      >
+        <fieldset className="govuk-fieldset">
+          <legend className="govuk-fieldset__legend">
+            <div className="govuk-accordion__section-header">
+              <h2 className="govuk-accordion__section-heading govuk-fieldset__heading">
+                <span
+                  className="govuk-accordion__section-button"
+                  id={`accordion-default-heading-${index + 1}`}
+                  data-cy={`cyAccordionButton-${''}`}
+                >
+                  Search options
+                </span>
+              </h2>
+            </div>
+          </legend>
+          <div
+            id={`accordion-default-content-${index + 1}`}
+            className="govuk-accordion__section-content"
+            data-testid="section-content"
+            aria-labelledby={`accordion-default-heading-${index + 1}`}
+            role="group"
+            data-cy={`cyAccordionContent-${''}`}
+          >
+            <Filters filters={filters} filterObj={filterObj} />
+          </div>
+        </fieldset>
+      </div>
+    </div>
+  );
+}
+
+function Filters({ filters, filterObj }) {
+  return (
+    <>
+      {filters?.map((filter, index) => (
+        <SearchFilterSelector
+          key={index}
+          index={index}
+          filter={filter}
+          filterObj={filterObj}
+        />
+      ))}
+
+      <SearchFilterDate index={filters.length} filterObj={filterObj} />
+    </>
   );
 }
