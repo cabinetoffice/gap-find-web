cb114f339e09bb757d17a3f3589a2cb0bc460a5e
IlyasBaqqari-CabinetOffice <ilyas.baqqari@cabinetoffice.gov.uk>
2024-01-23 16:23:34 +0000

    Adjust banner image

--- src/components/homepage/FundedByGovukBanner.tsx
+++ src/components/homepage/FundedByGovukBanner.tsx
@@ -3,10 +3,10 @@ export default function FundedByGovukBanner({ text }) {
   return (
     <div className="govuk-border govuk-!-margin-top-8">
       <Image
-        src="/assets/images/govuk-funding-logo.png"
+        src="/assets/images/govuk-funded-logo.png"
         alt="govuk funding logo"
         width="350"
-        height="70"
+        height="61"
       />
       <p className="govuk-body govuk-!-margin-top-8">{text}</p>
     </div>
