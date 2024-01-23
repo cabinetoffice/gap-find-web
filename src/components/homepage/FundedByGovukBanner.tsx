e25dcc029fd4d9f6dde3fd52f90dc0b0fc52d430
IlyasBaqqari-CabinetOffice <ilyas.baqqari@cabinetoffice.gov.uk>
2024-01-23 16:48:18 +0000

    Improve component modularity

--- src/components/homepage/FundedByGovukBanner.tsx
+++ src/components/homepage/FundedByGovukBanner.tsx
@@ -2,13 +2,15 @@ import Image from 'next/image';
 export default function FundedByGovukBanner({ text }) {
   return (
     <div className="govuk-border govuk-!-margin-top-9">
-      <Image
-        src="/assets/images/govuk-funded-logo.png"
-        alt="govuk funding logo"
-        width="350"
-        height="61"
-      />
-      <p className="govuk-body govuk-!-margin-top-8">{text}</p>
+      <div className="govuk-!-margin-bottom-4">
+        <Image
+          src="/assets/images/govuk-funded-logo.png"
+          alt="govuk funding logo"
+          width="350"
+          height="61"
+        />
+      </div>
+      {text ? <p className="govuk-body govuk-!-margin-top-8">{text}</p> : <></>}
     </div>
   );
 }
