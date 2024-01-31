b1fcff727ead6237d285e65026f3066f581e11c4
paul-lawlor-tco <paul.lawlor1@cabinetoffice.gov.uk>
2024-01-31 12:22:21 +0000

    Refactored tests to give desired redirect results.

--- __tests__/pages/apply/[pid].test.js
+++ __tests__/pages/apply/[pid].test.js
@@ -1,35 +1,25 @@
-import {
-  getServerSideProps,
-  getAdvertSchemeVersion,
-} from '../../../pages/apply/[pid]'; // Import your module here
+import { getServerSideProps } from '../../../pages/apply/[pid]'; // Import your module here
+
+import * as axios from 'axios';
+
+// Mock out all top level functions, such as get, put, delete and post:
+jest.mock('axios');
 
 // Mock the fetchEntry function
 jest.mock('../../../src/utils/contentFulPage.ts', () => ({
   fetchEntry: jest.fn(() =>
     Promise.resolve({
       props: {
-        grantDetail: { fields: { grantWebpageUrl: 'https://example.com' } },
+        grantDetail: {
+          fields: {
+            grantWebpageUrl: 'https://example.com',
+            label: 'example-slug',
+          },
+        },
       },
     }),
   ),
 }));
-
-// Mock the getAdvertSchemeVersion function
-// jest.mock('../../../pages/apply/[pid]', () => ({
-//   getAdvertSchemeVersion: jest.fn(() =>
-//     Promise.resolve({
-//       data: {
-//         schemeVersion: 1,
-//         internalApplication: false,
-//       },
-//     }),
-//   ),
-// }));
-
-jest.mock('../../../pages/apply/[pid]', () => ({
-  getAdvertSchemeVersion: jest.fn(),
-}));
-const mockGetAdvertSchemeVersion = jest.mocked(getAdvertSchemeVersion);
 
 const applicantUrlBackup = process.env.APPLY_FOR_A_GRANT_APPLICANT_URL;
 const mandatoryQsEnabledBackup =
@@ -39,6 +29,12 @@ process.env.NEW_MANDATORY_QUESTION_JOURNEY_ENABLED = 'false';
 
 describe('getServerSideProps', () => {
   it('should return a redirect object with the expected destination when new mandatory question feature flag is off ', async () => {
+    //this mocks getAdvertSchemeVersion()
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: { schemeVersion: 2, internalApplication: true },
+      }),
+    );
     process.env.APPLY_FOR_A_GRANT_APPLICANT_URL = 'applicantUrl';
     process.env.NEW_MANDATORY_QUESTION_JOURNEY_ENABLED = 'false';
 
@@ -54,23 +50,16 @@ describe('getServerSideProps', () => {
   });
 
   it('should return a redirect object with the expected destination when new mandatory question feature flag is on ', async () => {
-    mockGetAdvertSchemeVersion.mockResolvedValueOnce({
-      status: 200,
-      data: { schemeVersion: 2, internalApplication: true },
-    }),
-      (process.env.APPLY_FOR_A_GRANT_APPLICANT_URL = 'applicantUrl');
+    //this mocks getAdvertSchemeVersion()
+    axios.get.mockImplementation(() =>
+      Promise.resolve({
+        data: { schemeVersion: 2, internalApplication: true },
+      }),
+    );
+    process.env.APPLY_FOR_A_GRANT_APPLICANT_URL = 'applicantUrl';
     process.env.NEW_MANDATORY_QUESTION_JOURNEY_ENABLED = 'true';
     const context = { params: { pid: 'your-path' } };
     const result = await getServerSideProps(context);
-
-    // jest.mock('../../../pages/apply/[pid]', () => ({
-    //   getAdvertSchemeVersion: jest.fn(() =>
-    //     Promise.resolve({
-    //       status: 200,
-    //       data: { schemeVersion: 2, internalApplication: true },
-    //     }),
-    //   ),
-    // }));
 
     expect(result).toEqual({
       redirect: {
