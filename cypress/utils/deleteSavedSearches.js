bb61ef4da8c6afe9be1ec014e2f7e44b3be4caac
JamieGunnCO <jamie.gunn@cabinetoffice.gov.uk>
2024-04-11 15:09:19 +0100

    delete unused cypress dir

--- cypress/utils/deleteSavedSearches.js
+++
@@ -1,9 +0,0 @@
-/* eslint no-undef: 0 */
-export default function deleteSavedSearches() {
-  cy.task('dbQuery', {
-    query: `DELETE FROM saved_search`,
-    connection: Cypress.env('DATABASE_URL'),
-  }).then((queryResponse) => {
-    console.log('queried response', queryResponse);
-  });
-}
